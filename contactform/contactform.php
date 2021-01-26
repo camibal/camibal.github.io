e0fe9462301bd70cb8a96e185d758d6f1a5788c3
Camilo Ballesteros <ballesteroscamilo72@gmail.com>
2021-01-26 12:11:24 -0500

    update wpp message

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,5 +1,4 @@
 <?php
-
 $name = $_POST['name'];
 $message = $_POST['message'];
-echo "<script>window.open('https://wa.me/+573202075715?text=Nombre: $name Mensaje: $message'); location.reload();</script>";
+echo "'Nombre: '+$message + 'Mensaje'+$name";
