999e91c8016870df96d6dfe980ca5a1e730de026
Camilo Ballesteros <ballesteroscamilo72@gmail.com>
2021-01-26 13:00:34 -0500

    fix: contact form

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,119 +1,47 @@
-jQuery(document).ready(function ($) {
-  "use strict";
-
-  //Contact
-  $('form.contactForm').submit(function () {
-    var f = $(this).find('.form-group'),
-      ferror = false,
-      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
-
-    f.children('input').each(function () { // run all inputs
-
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
-
-      if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
-
-        switch (rule) {
-          case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'email':
-            if (!emailExp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'checked':
-            if (!i.is(':checked')) {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'regexp':
-            exp = new RegExp(exp);
-            if (!exp.test(i.val())) {
-              ferror = ierror = true;
-            }
-            break;
-        }
-        i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-      }
-    });
-    f.children('textarea').each(function () { // run all inputs
-
-      var i = $(this); // current input
-      var rule = i.attr('data-rule');
-
-      if (rule !== undefined) {
-        var ierror = false; // error flag for current input
-        var pos = rule.indexOf(':', 0);
-        if (pos >= 0) {
-          var exp = rule.substr(pos + 1, rule.length);
-          rule = rule.substr(0, pos);
-        } else {
-          rule = rule.substr(pos + 1, rule.length);
-        }
-
-        switch (rule) {
-          case 'required':
-            if (i.val() === '') {
-              ferror = ierror = true;
-            }
-            break;
-
-          case 'minlen':
-            if (i.val().length < parseInt(exp)) {
-              ferror = ierror = true;
-            }
-            break;
-        }
-        i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
-      }
-    });
-    if (ferror) return false;
-    else var str = $(this).serialize();
-    var action = $(this).attr('action');
-    if (!action) {
-      action = 'contactform/contactform.php';
-    }
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function (msg) {
-        window.open('https://wa.me/+573202075715?text=' + msg);
-        location.reload();
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
-
-      }
-    });
+function isMobile() {
+  if (sessionStorage.desktop)
     return false;
-  });
+  else if (localStorage.mobile)
+    return true;
+  var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+  for (var i in mobile)
+    if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+  return false;
+}
 
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '+573202075715';
+
+formulario.addEventListener('submit', (event) => {
+  event.preventDefault()
+  // buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+  // buttonSubmit.disabled = true
+  setTimeout(() => {
+    let name = document.querySelector('#name').value;
+    let message = document.querySelector('#message').value;
+
+    if (name == null || name == 0 || name == /^\s+$/.test(name)) {
+      // Si no se cumple la condicion...
+      alert('[ERROR] El campo nombre debe estar lleno');
+      return false;
+    }
+    else if (message == null || message == 0 || /^\s+$/.test(message)) {
+      // Si no se cumple la condicion...
+      alert('[ERROR] El campo mensaje debe estar lleno');
+      return false;
+    }
+
+
+    let mensaje = 'send?phone=' + telefono + '&text=*_Formulario CODE_*%0A*¿Cual es tu nombre?*%0A' + name + '%0A*¿Cual es tu mensaje?*%0A' + message
+    if (isMobile()) {
+      window.open(urlMobile + mensaje, '_blank')
+    } else {
+      window.open(urlDesktop + mensaje, '_blank')
+    }
+    buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+    buttonSubmit.disabled = false
+  }, 100);
 });
